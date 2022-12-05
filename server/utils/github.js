59fc9a720cf3ad74fead750a0e27020ca412daed
nick-echevarria <nick.echev@gmail.com>
2022-12-04 23:37:14 -0500

    minor: tweaked gitignore

--- server/utils/github.js
+++
@@ -1,8 +0,0 @@
-const HEADERS = {
-  headers: {
-    Authorization:
-      'token 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',
-  },
-};
-
-module.exports = HEADERS; 
